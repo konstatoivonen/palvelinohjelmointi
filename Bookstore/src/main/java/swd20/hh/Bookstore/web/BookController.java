3d7b73b36ea757ec9db5fccf27debe6eb90cb0ab
Konsta Toivonen <konsta.toivonen@protonmail.com>
2020-02-09 12:15:19 +0200

    Created Book & BookController Classes

---
+++ Bookstore/src/main/java/swd20/hh/Bookstore/web/BookController.java
@@ -0,0 +1,15 @@
+package swd20.hh.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value="/index", method=RequestMethod.GET)
+public class BookController {
+	
+
+}
