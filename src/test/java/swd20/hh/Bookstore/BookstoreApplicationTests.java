bcba744dee000e67eb1f8cd8f4b867fc8981d3d0
Konsta Toivonen <konsta.toivonen@protonmail.com>
2020-03-31 19:44:01 +0300

    Trying to repair the directory for heroku deployment

--- src/test/java/swd20/hh/Bookstore/BookstoreApplicationTests.java
+++ src/test/java/swd20/hh/Bookstore/BookstoreApplicationTests.java
@@ -3,6 +3,7 @@ package swd20.hh.Bookstore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith (SpringRunner.class)
 @SpringBootTest
 class BookstoreApplicationTests {
 
